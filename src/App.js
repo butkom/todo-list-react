07b0942ff718e8e3f3ea55a69141f458ca14cdf6
butko <maksimbutko@nixsolutions.com>
2020-01-21 18:45:41 +0200

    Add carcase for render list of elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Ul extends React.Component{
+    render() {
+        return (
+            <ul>
+                {this.props.liElements}
+            </ul>
+        );
+    }
+}
+
+class Li extends React.Component{
+    render() {
+        return (
+            <li>
+                <span>li element</span>
+            </li>
+        );
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Ul liElements={<Li />} />
     </div>
   );
 }
