51ddc61a5087847684f92d280fe2f9f92c12cf05
butko <maksimbutko@nixsolutions.com>
2020-01-22 12:58:44 +0200

    Move App from function to class and use initialTodo as state

--- src/App.js
+++ src/App.js
@@ -19,32 +19,41 @@ class Tr extends React.Component
     };
 }
 
-function App() {
-    const listRows = initialTodo.map((item) =>
-        <Tr key={item} content={item} />
-    );
+class App extends React.Component
+{
+    constructor(props) {
+        super(props);
 
-  return (
-    <table className="App">
-        <thead>
-            <tr>
-                <th>Action</th>
-                <th>Content</th>
-                <th>Remove</th>
-                <th>Status</th>
-            </tr>
-            <tr>
-                <td>#</td>
-                <td><input type="text" name="content" /></td>
-                <td>#</td>
-                <td>Status</td>
-            </tr>
-        </thead>
-        <tbody>
-            {listRows}
-        </tbody>
-    </table>
-  );
+        this.state = {initialTodo}
+    }
+
+    render() {
+        return (
+            <table className="App">
+                <thead>
+                <tr>
+                    <th>Action</th>
+                    <th>Content</th>
+                    <th>Remove</th>
+                    <th>Status</th>
+                </tr>
+                <tr>
+                    <td>#</td>
+                    <td><input type="text" name="content" /></td>
+                    <td>#</td>
+                    <td>Status</td>
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.initialTodo.map((item) =>
+                            <Tr key={item} content={item} />
+                        )
+                    }
+                </tbody>
+            </table>
+        );
+    }
 }
 
 export default App;
