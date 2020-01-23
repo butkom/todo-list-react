897a2e7a45bff3570085708de28873d892b80f5d
butko <maksimbutko@nixsolutions.com>
2020-01-23 16:28:56 +0200

    Add edit todo functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 const todos = [
     {
         content: 'task 1',
-        checked: 1
+        checked: 1,
+        updateInProgress: 0
     },
     {
         content: 'task 2',
-        checked: 0
+        checked: 0,
+        updateInProgress: 0
     },
 ];
 
@@ -21,7 +23,10 @@ class App extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.removeTodo = this.removeTodo.bind(this);
-        this.handleCheckboxChange = this.handleCheckboxChange.bind(this)
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+        this.handleChangeTodo = this.handleChangeTodo.bind(this);
+        this.handleDoubleClick = this.handleDoubleClick.bind(this);
+        this.handleInputBlur = this.handleInputBlur.bind(this);
     }
 
     handleSubmit(event) {
@@ -60,8 +65,35 @@ class App extends React.Component {
         todos[index].checked = value;
 
         this.setState({todos});
-        
+    }
+
+    handleChangeTodo(event) {
+        const target = event.target;
+        const index = target.name;
+        const value = target.value;
+
+        let todos = this.state.todos;
+        todos[index].content = value;
+
+        this.setState({todos});
+    }
+
+    handleDoubleClick(index) {
+        let todos = this.state.todos;
+
+        todos[index].updateInProgress = 1;
+
+        this.setState({todos});
+    }
+
+    handleInputBlur(index) {
         console.log(index);
+        // let todos = this.state.todos;
+        // if (todos[index].updateInProgress) {
+        //     todos[index].updateInProgress = 0;
+        //
+        //     this.setState({todos});
+        // }
     }
 
     render() {
@@ -91,13 +123,30 @@ class App extends React.Component {
                             <td>
                                 <input type="checkbox"
                                        name={index}
-                                       onClick={this.handleCheckboxChange}
+                                       onChange={this.handleCheckboxChange}
                                        checked={item.checked}
                                 />
                             </td>
-                            <td><span style={{textDecoration: item.checked ? 'line-through' : 'none'}}>{item.content}</span></td>
                             <td>
-                                <button onClick={() => {this.removeTodo(index);}}>
+                                <div
+                                    onDoubleClick={() => this.handleDoubleClick(index)}
+                                    style={{textDecoration: item.checked ? 'line-through' : 'none', display: item.updateInProgress ? 'none' : 'block'}}
+                                >
+                                    {item.content}
+                                </div>
+                                <div style={{display: item.updateInProgress ? 'block' : 'none'}}>
+                                    <input
+                                        name={index}
+                                        type="text"
+                                        onChange={this.handleChangeTodo}
+                                        value={this.state.todos[index].content}
+                                    />
+                                </div>
+                            </td>
+                            <td>
+                                <button onClick={() => {
+                                    this.removeTodo(index);
+                                }}>
                                     <span>x</span>
                                 </button>
                             </td>
