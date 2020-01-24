e7dbd4017b87438f9b72a770d195d3145c793918
butko <maksimbutko@nixsolutions.com>
2020-01-24 12:18:08 +0200

    Add bootstap-css and node-sass to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import TBodyRows from './TBodyRows.js'
 import NewTodoElement from './NewTodoElement.js'
 
