c7e450cd9450024a73f311b22d0521b7f73ba977
butko <maksimbutko@nixsolutions.com>
2020-01-27 17:00:49 +0200

    Add provider with store to App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -8,18 +9,12 @@ import createTasks from './reducers/tasks'
 
 const store = createStore(createTasks);
 
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch({
-    type: 'ADD_TASK',
-    payload: {
-        content: 'task 3',
-        checked: 0,
-        updateInProgress: 0
-    }
-});
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
