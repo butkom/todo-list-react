db80dcf07e98f56ec36d1c228cf6fa10939f1adf
butko <maksimbutko@nixsolutions.com>
2020-01-29 17:44:31 +0200

    Refactor task list

--- src/ToDoElements.js
+++ src/ToDoElements.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import UpdateTodo from  './UpdateTodo.js';
+
+import UpdateTodoElement from  './UpdateTodoElement';
+import TodoElement from './TodoElement'
 
 class ToDoElements extends React.Component {
     render() {
@@ -8,31 +10,8 @@ class ToDoElements extends React.Component {
                 <div className='form-check'>
                     {
                         this.props.updateInProgress ?
-                            <div>
-                                <input
-                                    className='edit-todo-element'
-                                    autoFocus={true}
-                                    onBlur={this.props.handleOnBlur(this.props.item)}
-                                    name={this.props.index}
-                                    type='text'
-                                    ref={(input) => {this.taskInput = input}}
-                                    onChange={this.props.handleChangeTodo(this.props.item, this.taskInput)}
-                                    value={this.props.item.content}
-                                />
-                            </div>
-                                :
-                            <div>
-                            <label className='form-check-label'>
-                                <input className='checkbox'
-                                       type='checkbox'
-                                       name={this.props.index}
-                                       onChange={this.props.handleCheckboxChange(this.props.item)}
-                                       checked={this.props.item.checked}
-                                />
-                                <i className='input-helper'></i>
-                            </label>
-                                <span  onDoubleClick={this.props.handleDoubleClick(this.props.item)}>{this.props.item.content}</span>
-                            </div>
+                            <UpdateTodoElement item={this.props.item} handleChangeTodo={this.props.handleChangeTodo} handleOnBlur={this.props.handleOnBlur}/> :
+                            <TodoElement item={this.props.item} handleCheckboxChange={this.props.handleCheckboxChange} handleDoubleClick={this.props.handleDoubleClick} />
                     }
                 </div>
                 <i className='remove mdi mdi-close-circle-outline' onClick={this.props.removeTodo(this.props.item)}></i>
---
+++ src/UpdateTodoElement.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class UpdateTodoElement extends React.Component {
+    render() {
+        return(
+            <div>
+                <input
+                    className='edit-todo-element'
+                    autoFocus={true}
+                    onBlur={this.props.handleOnBlur(this.props.item)}
+                    type='text'
+                    ref={(input) => {this.taskInput = input}}
+                    onChange={this.props.handleChangeTodo(this.props.item, this.taskInput)}
+                    value={this.props.item.content}
+                />
+            </div>
+        );
+    }
+}
+
+export default UpdateTodoElement;
